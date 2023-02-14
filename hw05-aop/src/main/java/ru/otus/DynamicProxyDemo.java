9fc3e67beacfe4b20079607e7d9af43404131f4a
fedbon <fbondarev1990@gmail.com>
2023-02-14 21:14:15 +0300

    hw05-aop

--- hw05-aop/src/main/java/ru/otus/DynamicProxyDemo.java
+++ hw05-aop/src/main/java/ru/otus/DynamicProxyDemo.java
@@ -3,10 +3,10 @@ package ru.otus;
 public class DynamicProxyDemo {
     public static void main(String[] args) throws ClassNotFoundException {
 
-        var testLoggingClass = Ioc.createTestLoggingClass();
-        testLoggingClass.calculation(Integer.MAX_VALUE);
-        testLoggingClass.calculation(4, 10); //there is no logging here
-        testLoggingClass.calculation(1, 8, "testParam");
-        testLoggingClass.calculation("testParam");
+        var testLogging = Ioc.createTestLoggingClass();
+        testLogging.calculation(Integer.MAX_VALUE);
+        testLogging.calculation(4, 10); //there is no logging here
+        testLogging.calculation(1, 8, "testParam");
+        testLogging.calculation("testParam");
     }
 }
