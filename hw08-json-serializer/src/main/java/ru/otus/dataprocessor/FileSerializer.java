b3f92c4027d6e7659ae667ca4268740760545b4e
fedbon <fbondarev1990@gmail.com>
2023-03-26 18:05:28 +0300

    hw08-json-serializer

---
+++ hw08-json-serializer/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -0,0 +1,37 @@
+package ru.otus.dataprocessor;
+
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class FileSerializer implements Serializer {
+
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void serialize(Map<String, Double> data) {
+        var sorted = getSorted(data);
+        writeIntoFile(sorted);
+    }
+
+    private Map<String, Double> getSorted(Map<String, Double> data) {
+        return new TreeMap<>(data);
+    }
+
+    private void writeIntoFile(Map<String, Double> data) {
+        var gson = new Gson();
+        try (var writer = new FileWriter(fileName)) {
+            gson.toJson(data, writer);
+            writer.flush();
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
+    }
+}
