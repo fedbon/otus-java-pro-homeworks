38d5be018d4ededb191b7572d3117f1a8d4652c3
fedbon <fbondarev1990@gmail.com>
2023-06-11 15:24:32 +0300

    hw14-spring-data-jdbc

--- hw14-spring-data-jdbc/src/main/java/ru/otus/crm/repository/ClientRepository.java
+++ hw14-spring-data-jdbc/src/main/java/ru/otus/crm/repository/ClientRepository.java
@@ -2,12 +2,9 @@ package ru.otus.crm.repository;
 
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import ru.otus.crm.model.Client;
 import java.util.List;
 
-@Repository
 public interface ClientRepository extends CrudRepository<Client, Long> {
-
     List<Client> findAll();
 }
